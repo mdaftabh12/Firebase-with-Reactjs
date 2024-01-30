a4a7e00b4424801ee91bf3f40b31320db8245e46
Md Aftab <mdaftabmth22@gmail.com>
2024-01-30 08:38:24 +0530

    Authentication With Email And Password

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import { getDatabase, ref, set } from "firebase/database";
-import { app } from "./firebase";
+//=============  Realtime Database ================//
 
+// import { getDatabase, ref, set } from "firebase/database";
+// import { app } from "./firebase";
+
+// import "./App.css";
+
+// const db = getDatabase(app);
+
+// function App() {
+//   const putData = () => {
+//     set(ref(db, "users/aftab"), {
+//       id: 1,
+//       name: "Al-Aftab",
+//       age: 23,
+//     });
+//   };
+//   return (
+//     <div className="App">
+//       <h1>Firebase react app</h1>
+//       <button onClick={putData}>Put Data</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//=============  Authentication With Email And Password  ================//
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "./firebase";
 import "./App.css";
 
-const db = getDatabase(app);
+const auth = getAuth(app);
 
 function App() {
-  const putData = () => {
-    set(ref(db, "users/aftab"), {
-      id: 1,
-      name: "Al-Aftab",
-      age: 23,
-    });
+  const signupUser = () => {
+    createUserWithEmailAndPassword(auth, "mdaftab@gmail.com", "Aftabh12").then(
+      (value) => {
+        console.log(value);
+      }
+    );
   };
   return (
     <div className="App">
       <h1>Firebase react app</h1>
-      <button onClick={putData}>Put Data</button>
+      <button onClick={signupUser}>Put Data</button>
     </div>
   );
 }
